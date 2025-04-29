f8bb5685998b6f9b1e3e55801814af1ce5f9934d
KRISHNAKUMAR-N-04 <krishnakumarnagarajan04@gmail.com>
2025-04-29 09:03:31 +0530

    Updates on authentication

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
+
 
 const Hero = () => {
+
+    const navigate = useNavigate();
+
+
   return (
     <div className='flex flex-col border border-gray-400 sm:flex-row'>
         {/* Hero left side */}
@@ -13,7 +19,7 @@ const Hero = () => {
                 </div>
                 <h1 className='text-3xl leading-relaxed sm:py-3 lg:text-5xl prata-regular'>Latest Arrivals</h1>
                 <div className='flex items-center gap-2'>
-                    <p className='text-sm font-semibold md:text-base'>SHOP NOW</p>
+                    <button className='text-sm font-semibold md:text-base' onClick={()=>{navigate('/collection')}}>SHOP NOW</button>
                     <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                 </div>
             </div>
