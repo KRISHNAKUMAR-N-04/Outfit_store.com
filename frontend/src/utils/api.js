dff11d16a02b0055376c9e7f2c12ec8bd839c269
KRISHNAKUMAR-N-04 <krishnakumarnagarajan04@gmail.com>
2025-05-22 01:19:22 +0530

    Updates

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,2 +1,2 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
+const BASE_URL = "https://outfit-store-com.onrender.com" || 'http://localhost:5000';
 export default BASE_URL;
