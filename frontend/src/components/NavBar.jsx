13d57ee87cbc6f08a6427d75b2e02955679cb32d
KRISHNAKUMAR-N-04 <krishnakumarnagarajan04@gmail.com>
2025-05-22 00:22:16 +0530

    Changes to search

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -42,7 +42,7 @@
             </ul>
             <div className='flex items-center gap-6'>
                 <img 
-                    onClick={() => setShowSearch(true)} 
+                    onClick={() => {navigate('/collection')}} 
                     src={assets.search_icon} 
                     className='w-5 cursor-pointer' 
                     alt="Search Products" 
